efaf939edc3b3260bcf540007da428e43f42d61b
Tess Huang <tesshuangxj@yahoo.com>
2021-03-29 22:28:19 -0700

    Update header with link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 interface HeaderProps {
   title: string;
   logoImage: string;
   user: string;
 }
 
-const Header = ({ logoImage, title, user }: HeaderProps) => {
+const Header: React.FC<HeaderProps> = ({ logoImage, title, user }) => {
   return (
     <header>
-      <img src={logoImage} alt={`${title}-logo`} />
+      <Link to="/">
+        <img src={logoImage} alt={`${title}-logo`} />
+      </Link>
       <h1>{title}</h1>
-      <div>Welcome {user}</div>
+      <div>
+        <Link to="/profile">Welcome {user}</Link>
+      </div>
     </header>
   );
 };
