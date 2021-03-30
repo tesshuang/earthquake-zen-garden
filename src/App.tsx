528237d2c8e92579d6f0329c61ea2dadc0444552
Tess Huang <tesshuangxj@yahoo.com>
2021-03-29 21:04:36 -0700

    Add header to app and update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
+import dataJson from './data.json';
 import './App.css';
 
 const App = () => {
+  const { site, profile, data } = dataJson;
+
   return (
     <div className="App">
+      <Header
+        logoImage={site.logoImage}
+        title={site.title}
+        user={profile.firstName}
+      />
       <h1>React app</h1>
     </div>
   );
