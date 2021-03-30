5792ce0b169575b25ae162fc1a3c98e2fd91dce3
Tess Huang <tesshuangxj@yahoo.com>
2021-03-29 20:23:58 -0700

    Update package and add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,7 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.(js|jsx)/,
-      //   exclude: /node_modules/,
-      //   use: ['babel-loader'],
-      // },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
