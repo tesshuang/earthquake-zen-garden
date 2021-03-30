4e08deb435994ef9ef3308b834f351a9b590557a
Tess Huang <tesshuangxj@yahoo.com>
2021-03-29 22:29:37 -0700

    Update app with profile comp and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Layout from './components/Layout';
+import Profile from './components/Profile';
 import dataJson from './data.json';
 import './App.css';
 
-const App = () => {
+const App: React.FC = () => {
   const { site, profile, data } = dataJson;
 
   return (
-    <div className="App">
-      <Header
-        logoImage={site.logoImage}
-        title={site.title}
-        user={profile.firstName}
-      />
-      <h1>React app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header
+          logoImage={site.logoImage}
+          title={site.title}
+          user={profile.firstName}
+        />
+        <Layout>
+          <Switch>
+            <Route path="/">
+              <h1>Home</h1>
+            </Route>
+            <Route path="/detail">
+              <h1>Detail view</h1>
+            </Route>
+            <Route path="/profile">
+              <Profile profile={profile} />
+            </Route>
+          </Switch>
+        </Layout>
+      </div>
+    </Router>
   );
 };
 
